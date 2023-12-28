3e8e91df7677f8e6eaa756bc821ddc154be02479
nespinoza2000 <113635313+nespinoza2000@users.noreply.github.com>
2023-12-28 17:44:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from lib.check_passw import check_user
 from model.handle_db import HandleDB
 import mysql.connector
 
+
 app = FastAPI()
 
 template = Jinja2Templates(directory="./view")
@@ -87,6 +88,28 @@ def verMat(req: Request):
     print(materiales)
     return template.TemplateResponse("ver_materiales.html", {"request": req, "materiales": materiales})
 
+
+@app.get("/EditarM/{id_del_material}")
+def edit_mat(
+    req: Request,
+    id_del_material: int,
+    nombrematerial: str = Form(...),
+    desc: str = Form(...),
+    precio: float = Form(...),
+    cantidad: int = Form(...)
+):
+    n_data_mat = {
+        "id": id_del_material,
+        "NombreMaterial": nombrematerial,
+        "DescripcionMat": desc,
+        "Precio": precio,
+        "Cantidad": cantidad
+    }
+    handle_db = HandleDB()
+    handle_db.update_mat(n_data_mat)
+    del handle_db
+    return RedirectResponse(url="/verMat", status_code=302)
+
 @app.get("/registrarCliente")  
 def registro_cli(req: Request):
     return template.TemplateResponse("clientes.html", {"request": req})
@@ -95,17 +118,30 @@ def registro_cli(req: Request):
 async def cargar_cli(
     req: Request,
     NombreCliente: str = Form(...),
-    RUC: str = Form(...)  # Asegúrate de que estos campos coincidan con el formulario HTML
+    RUC: str = Form(...),
+    direccion: str = Form(...),
+    telefono: int = Form(...)
 ):
     data_cliente = {
         "NombreCliente": NombreCliente,
-        "RUC": RUC
+        "RUC": RUC,
+        "Direccion": direccion,
+        "Telefono": telefono
     }
     handle_db = HandleDB()
-    handle_db.insert_cliente(data_cliente)
+    cliente_registrado = handle_db.insert_cliente(data_cliente)  # Solo una vez
     del handle_db
+
+    mensaje = ""
+    if cliente_registrado:
+        mensaje = "Un nuevo cliente ha sido registrado con éxito"
+    else:
+        mensaje = "El cliente ya existe. Por favor, registre un nuevo cliente"
+
     return template.TemplateResponse(
-        "employd.html", {"request": req, "data_user": True})
+        "employd.html", {"request": req, "data_user": True, "mensaje": mensaje}
+    )
+
 
 @app.get("/verCliente")
 def verClient(req: Request):
@@ -113,6 +149,30 @@ def verClient(req: Request):
     clientes = handle_db.get_cliente()
     return template.TemplateResponse("ver_clientes.html", {"request": req, "clientes": clientes})
 
+@app.get("/EditarCliente/{id_del_cliente}")
+def show_edit_cliente(req: Request, id_del_cliente: int):
+    handle_db = HandleDB()
+    cliente = handle_db.get_cliente_by_id(id_del_cliente)  # Cambiar a get_cliente_by_id para obtener un cliente específico
+    del handle_db
+    return template.TemplateResponse("editar_cliente.html", {"request": req, "cliente": cliente})
+
+@app.post("/EditarCliente/{id_del_cliente}/update")
+def update_client(req: Request, id_del_cliente: int, NombreCliente: str = Form(...), RUC: str = Form(...), direccion: str = Form(...), telefono: int = Form(...)):
+    # Procesar la actualización del cliente
+    new_data_cliente = {
+        "id_del_cliente": id_del_cliente,
+        "NombreCliente": NombreCliente,
+        "RUC": RUC,
+        "Direccion": direccion,
+        "Telefono": telefono
+    }
+    handle_db = HandleDB()
+    handle_db.update_cliente(new_data_cliente)
+    del handle_db
+    return RedirectResponse(url="/verCliente", status_code=302)
+
+
 @app.get("/registrarPro")
 def registro_cli(req: Request):
     return template.TemplateResponse("proveedores.html", {"request": req})
+
